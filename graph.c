12e5018c3848f5f57d2b5c2e83d3bfe4e5773411
Vinicius Costa <costacastrovinicius7@gmail.com>
2019-11-28 11:31:36 -0300

    feature(graph): Graph create, destroy and insert functions implemented

---
+++ graph.c
@@ -0,0 +1,79 @@
+#ifndef _GRAPH_C_
+#define _GRAPH_C_
+
+#include "graph.h"
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct _adjacency_matrix_ {
+    int vertices;
+    int **matriz;
+};
+
+GraphAdjacencyMatrix *createGraphAdjacencyMatrix(int vertices){
+    int i, j;
+    GraphAdjacencyMatrix *graph;
+    
+    if (vertices <= 0)
+        return NULL;
+
+    graph = (GraphAdjacencyMatrix *) malloc(sizeof(GraphAdjacencyMatrix));
+    if (!graph)
+        return NULL;
+    graph->vertices = vertices;
+    graph->matriz = (int **) malloc(graph->vertices * sizeof(int *));
+    if (!graph->matriz){
+        free(graph);
+        return NULL;
+    }
+    
+    for (i = 0; i < graph->vertices; i++){
+        graph->matriz[i] = (int *) malloc(graph->vertices * sizeof(int));
+        if (!graph->matriz[i]){
+            for (j = 0; j < i; j++)
+                free(graph->matriz[j]);
+            free(graph->matriz);
+            free(graph);
+            return NULL;
+        }
+    }
+
+    for (i = 0; i < graph->vertices; i++){
+        for (j = 0; j < graph->vertices; j++){
+            graph->matriz[i][j] = 0;
+        }
+    }
+
+    return graph;
+}
+
+int destroyGraphAdjacencyMatrix(GraphAdjacencyMatrix *graph){
+    int i;
+
+    if (!graph)
+        return false;
+
+    for (i = 0; i < graph->vertices; i++)
+        free(graph->matriz[i]);
+    free(graph->matriz);
+    free(graph);
+
+    return true;
+}
+
+int insertGraphAdjacencyMatrix(GraphAdjacencyMatrix *graph, int vertexA, int vertexB, int weight, int directioned){
+    if (!graph)
+        return false;
+    if (vertexA < 0)
+        return false;
+    if (vertexA < 0)
+        return false;
+    
+    graph->matriz[vertexA][vertexB] = weight;
+    if (!directioned)
+        graph->matriz[vertexB][vertexA] = weight;
+
+    return true;
+}
+
+#endif
---
+++ graph.h
@@ -0,0 +1,16 @@
+#ifndef _GRAPH_H_
+
+typedef struct _adjacency_matrix_ GraphAdjacencyMatrix;
+
+
+GraphAdjacencyMatrix *createGraphAdjacencyMatrix(int);
+int destroyGraphAdjacencyMatrix(GraphAdjacencyMatrix *);
+int insertGraphAdjacencyMatrix(GraphAdjacencyMatrix *, int, int, int);
+
+#else
+
+extern GraphAdjacencyMatrix *createGraphAdjacencyMatrix(int);
+extern int destroyGraphAdjacencyMatrix(GraphAdjacencyMatrix *);
+extern int insertGraphAdjacencyMatrix(GraphAdjacencyMatrix *, int, int, int);
+
+#endif
