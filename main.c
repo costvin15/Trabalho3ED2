502fb9fcd5799bad5924726d8e52e8a2b9d995e2
Vinicius Costa <costacastrovinicius7@gmail.com>
2019-12-06 11:35:09 -0300

    feature(utils): readFileAndPopulateGraphAdjacencyMatrix implemented

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+#include <stdio.h>
 #include "graph.h"
 #include "utils.h"
 
-int main(int argc, char **argv){
-    readFileAndPopulateGraphAdjacencyMatrix();
+int main(void){
+    int i, j;
+    GraphAdjacencyMatrix *graph;
+    
+    graph = readFileAndPopulateGraphAdjacencyMatrix();
+    for (i = 0; i < getVertexCountGraphAdjacencyMatrix(graph); i++){
+        for (j = 0; j < getVertexCountGraphAdjacencyMatrix(graph); j++)
+            printf("%d ", getVertexGraphAdjacencyMatrix(graph, i, j));
+        printf("\n");
+    }
+
+    return 0;
 }
--- utils.c
+++ utils.c
@@ -1,9 +1,10 @@
-#ifndef _UTILS_H_
-#define _UTILS_H_
+#ifndef _UTILS_C_
+#define _UTILS_C_
 
 #include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 char *readFile(char *filename){
     FILE *file;
@@ -38,7 +39,56 @@ char *readFile(char *filename){
 }
 
 GraphAdjacencyMatrix *readFileAndPopulateGraphAdjacencyMatrix(){
-    printf("%s\n", readFile("inputs/test"));
+    int i, j;
+    int numlines;
+    char *lines;
+    char *linessaveptr;
+    char *content;
+    char *numbers;
+    int **graphcontent;
+    GraphAdjacencyMatrix *graph;
+    
+    content = readFile("inputs/test");
+    if (!content)
+        return NULL;
+
+    lines = __strtok_r(content, "\n", &linessaveptr);
+    numlines = atoi(lines);
+    lines = __strtok_r(NULL, "\n", &linessaveptr);
+
+    graphcontent = (int **) malloc(numlines * sizeof(int *));
+    if (!graphcontent)
+        return NULL;
+
+    i = 0;
+    while (lines){
+        j = 0;
+        graphcontent[i] = (int *) malloc(3 * sizeof(int));
+        graphcontent[i][2] = 0;
+        if (!graphcontent[i]){
+            for (; j < i; j++)
+                free(graphcontent[i]);
+            return NULL;
+        }
+
+        numbers = strtok(lines, " ");
+        while (numbers){
+            graphcontent[i][j++] = atoi(numbers);
+            numbers = strtok(NULL, "\n");
+        }
+        lines = __strtok_r(NULL, "\n", &linessaveptr);
+        i++;
+    }
+
+    graph = createGraphAdjacencyMatrix(numlines);
+    if (!graph)
+        return NULL;
+
+    for (i = 0; i < numlines; i++)
+        if(!insertGraphAdjacencyMatrix(graph, graphcontent[i][0] - 1, graphcontent[i][1] - 1, graphcontent[i][2], graphcontent[i][2] == 0 ? 0 : 1))
+            return NULL;
+
+    return graph;
 }
 
 #endif
